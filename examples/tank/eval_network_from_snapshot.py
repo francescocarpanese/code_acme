6301865751abdcb7f87faa50bd84e8fc1b850ccf
Francesco <ciskogoal90@gmail.com>
2021-12-12 14:14:08 +0000

    Replay stored policy ongoing

--- examples/tank/eval_network_from_snapshot.py
+++ examples/tank/eval_network_from_snapshot.py
@@ -13,23 +13,30 @@ from acme import wrappers
 import matplotlib.pyplot as plt
 from environments.tank import tank
 from environments.tank import Tasks
+import pandas as pd
 
-N_max_steps = 42
 
 # Instantiate env
-env = tank.env(tend=2., task = Tasks.Step())
+env = tank.env(tend=2., task = Tasks.Step(), debug = True)
 # Env was store in single precisio hence need 
 # to use single precision to use saved net
+env = wrappers.CanonicalSpecWrapper(env, clip= True) # Clip actions by bounds
 env = wrappers.SinglePrecisionWrapper(env) 
-#environment = wrappers.StepLimitWrapper(env, N_max_steps) # Set max number of simulation time steps
-
 
 # Specification for network IO
 spec = specs.Array([2], dtype=np.float32)
 inputs = tf2_utils.add_batch_dim(tf2_utils.zeros_like(spec))
 
+store_path = '/root/acme/a7873ce0-5b38-11ec-ac5b-0242ac110002/'
+store_path = '/root/acme/d867b0be-5b39-11ec-ad8a-0242ac110002/'
+store_path = '/root/acme/f5a6e038-5b3d-11ec-a09d-0242ac110002/'
+store_path = '/root/acme/dae95774-5b3f-11ec-a7c0-0242ac110002/'
+store_path = '/root/acme/dae95774-5b3f-11ec-a7c0-0242ac110002/'
+
 # Load snapshot
-folder_path =  '/root/acme/d5a67d06-5abc-11ec-9c56-0242ac110002/snapshots/policy'
+folder_path =  store_path +'snapshots/policy'
+env_logs_path = store_path + '/logs/environment_loop/logs.csv'
+env_logs = pd.read_csv(env_logs_path)
 
 # Extract solution for debugging
 policy_net = tf.saved_model.load(folder_path)
@@ -48,31 +55,42 @@ actor = actors.FeedForwardActor(
     policy_network=behavior_network)
 
 # Storing action and observation for debugging
-a = np.zeros([1,N_max_steps])
-o = np.zeros([2,N_max_steps])
-r = np.zeros([1,N_max_steps])
-
-counter = 0
+#a_policy = np.zeros([1,N_max_steps])
 
 timestep = env.reset()
 while not timestep.last():
-    action = actor.select_action(timestep.observation)
+    # These actions are intended as 
+    action = actor.select_action(timestep.observation) 
     timestep = env.step(action)
-    a[:,counter] = action
-    o[:,counter] = timestep.observation
-    r[:,counter] = timestep.reward
-    counter += 1
 
-plt.title('Observation')
-plt.plot(np.transpose(o))
+data = env.get_packed_datadict()
+
+# Plot all time traces
+for key in [k for k in data.keys() if k != 'time']:
+    plt.plot(data['time'],data[key])
+    plt.xlabel(key)
+    plt.ylabel('time')
+    plt.show()
+
+
+plt.plot(env_logs.episodes, env_logs.episode_return)
+plt.xlabel('episodes')
+plt.ylabel('epsisode return')
 plt.show()
 
 
-plt.title('Reward')
-plt.plot(np.transpose(r))
+plt.plot(env_logs.episodes, env_logs.steps_per_second)
+plt.xlabel('episodes')
+plt.ylabel('sps')
 plt.show()
 
-
-plt.title('Actions')
-plt.plot(np.transpose(a))
+plt.plot(env_logs.episodes, env_logs.steps_per_second)
+plt.xlabel('episodes')
+plt.ylabel('sps')
 plt.show()
+
+plt.plot(env_logs.steps, env_logs.episode_return)
+plt.xlabel('steps')
+plt.ylabel('epsisode return')
+plt.show()
+
