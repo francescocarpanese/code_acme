5aa709aa2056ec347487a25cf28ebb2b5a93980a
Francesco <ciskogoal90@gmail.com>
2022-01-12 16:56:41 +0000

    linting and cleaning

--- environments/dm_control/utils/param.py
+++ environments/dm_control/utils/param.py
@@ -1,21 +1,29 @@
+"""Utils for parameters serelization/storing/reading"""
 import toml
 import numpy as np
 
 def overload_par_dict(par_dict, **kwargs):
+    """Overloads value in parameter dictionary from inputs"""
     for key, value in kwargs.items():
         if key in par_dict.keys():
-            par_dict[key] = np.asarray(value) if type(value) == list else value
+            par_dict[key] = (
+                np.asarray(value) if isinstance(value,list)
+                else value
+                )
 
 def write_config_file(default_par_list, par_dict, path, filename):
-    with open(path + filename + ".toml", 'w') as file:
-        for x in default_par_list:
-            file.write("#" + x.description + "\n")
-            toml.dump({x.name: par_dict[x.name]}, file)
+    """Writes toml file from parameter dictionary"""
+    with open(path + filename + ".toml", 'w', encoding='UTF-8') as file:
+        for parameter in default_par_list:
+            file.write("#" + parameter.description + "\n")
+            toml.dump({parameter.name: par_dict[parameter.name]}, file)
             file.write("\n")
 
 def get_par_from_config_file(path):
-    with open(path) as file:
+    """Returns parameter from toml file"""
+    with open(path, encoding='UTF-8') as file:
         return toml.load(file)
 
 def set_par_from_config_file(par_dict, path):
+    """Reads parameters from parameter file and overload parameter dictionary"""
     overload_par_dict(par_dict, **get_par_from_config_file(path))
--- environments/dm_control/utils/test_param.py
+++ environments/dm_control/utils/test_param.py
@@ -1,15 +1,18 @@
-from environments.dm_control.utils import param
+"""Test utils for parameters serializing/storing/reading"""
 from collections import namedtuple
-import numpy as np
 import os
+from environments.dm_control.utils import param
 
 def test_overloading():
+    """Test overloading of value in parameter dictionary"""
     dict_default = {'first': 1, 'second': [1.,2.]}
     dict_new = {'second': [2.,2.]}
     param.overload_par_dict(dict_default, **dict_new)
     assert all( dict_default['second'] == dict_new['second'])
 
 def test_config_file(tmpdir):
+    """ Test parameter configuration file handling"""
+    # Generate parameters with default value and descriptions
     par = namedtuple('par', 'name value description')
     default_par_list = [
         par('a',1.,'test float'),
@@ -19,23 +22,17 @@ def test_config_file(tmpdir):
     # Generate dictionary from parameter list
     par_dict = {x.name:x.value for x in default_par_list}
 
+    # Write parameters into toml configuration file
     fname = "tmp"
     path = str(tmpdir)
     full_path = path + fname + ".toml"
     param.write_config_file(default_par_list, par_dict, path, fname)
     assert os.path.exists(full_path), 'Configuration file not written'
 
-    # Load from config file
+    # Load parameters from config file
     par_from_file = param.get_par_from_config_file(full_path)
 
-    # Check read from file is the same as written to file
+    # Check read from file is the same as written data serialized into file file
     for key in par_from_file:
-        assert par_from_file[key] == par_dict[key], f'Parameter {key} different when read from file'
-
-    
-
-
-
-
-
-
+        assert par_from_file[key] == par_dict[key],\
+             f'Parameter {key} different when read from file'
